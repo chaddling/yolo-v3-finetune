232ee10dded2eaf7f6d477ab02ab2fb3e799ac15
Chad Gu <chad.m.gu@gmail.com>
2025-01-02 06:32:23 +0000

    Load pretrained model and replace the last layer

---
+++ yolo/model.py
@@ -0,0 +1,50 @@
+import os
+import torch
+
+from yolo.layers import Detect
+
+from typing import List, Tuple
+
+_anchors = [
+    (10, 13, 16, 30, 33, 23), # P3/8
+    (30, 61, 62, 45, 59, 119), # P4/16
+    (116, 90, 156, 198, 373, 326) # P5/32
+]
+_connect_from = (27, 22, 15)
+
+class PretrainedYOLOModel(torch.nn.Module):
+    def __init__(self, 
+        weights_dir: str, 
+        num_classes: int, # num_classes = # of classes in LVIS
+        anchors: List[Tuple[int]] = _anchors, 
+        training: bool = True
+    ):
+        super().__init__()
+
+        # Load custom weights, downloaded to `weights_dir`
+        # TODO add function to get the weights
+        self._model = torch.hub.load(
+            "ultralytics/yolov3", 
+            "custom", 
+            os.path.join(weights_dir, "yolov3.pt"), 
+            **{"autoshape": False if training else True} 
+        )
+        # Removes the top
+        self._model.model.model = self._model.model.model[:-1]
+
+        detect_layer = Detect(
+            nc=num_classes, 
+            anchors=anchors,
+            ch=[3], # TODO check if this is right
+        )
+        # These 4 attributes are needed in the BaseModel's _forward_once() instructions
+        detect_layer.i = len(self._model.model.model)
+        detect_layer.type = "yolo.models.Detect"
+        detect_layer.f = _connect_from
+        detect_layer.np = sum(x.numel() for x in detect_layer.parameters())
+
+        self._model.append(detect_layer)
+
+    def forward(self, x):
+        x = self._model(x)
+        return x
