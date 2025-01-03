f35fef087098f2ae81edcc6ecdedb10ddf841c17
Chad Gu <chad.m.gu@gmail.com>
2025-01-03 07:29:02 +0000

    WIP adding configurations

--- data/dataloader.py
+++ data/dataloader.py
@@ -1,19 +1,25 @@
 import os
 import glob
+import importlib
 import logging
 import pandas as pd
 import torch
 import torchvision.transforms.v2 as tv2
 
-from data.transforms import Resize, ColorJitter, RandomHorizontalFlip, RandomVerticalFlip
-
 from torch.utils.data import DataLoader, Dataset
 from torchvision.io import read_image
-from typing import List, Optional, Tuple, Union
+from typing import Dict, List, Optional, Tuple
 
 
-def create_dataloader() -> DataLoader:
-    pass
+def create_dataloader(config: Dict) -> DataLoader:
+    batch_size = config["dataloaer"]["batch_size"]
+    dataset_params = config["dataloader"]["dataset"]
+
+    cls = LVISDataset # TODO configure
+
+    dataset_params.pop("cls")
+    dataset = cls(**dataset_params)
+    return DataLoader(dataset, batch_size=batch_size)
 
 
 class BaseDataset(Dataset):
@@ -21,12 +27,7 @@ class BaseDataset(Dataset):
         self, 
         image_dir: str, 
         label_dir: str,
-        transforms: Optional[List[Union[torch.nn.Module, tv2.Transform]]] = [
-            Resize(size=640),
-            ColorJitter(brightness=0.1, contrast=0.1, hue=0.1, saturation=0.1), # set in config
-            RandomHorizontalFlip(p=0.1),
-            RandomVerticalFlip(p=0.1),
-        ],
+        transform_params: Optional[Dict[str, Dict]]
     ):
         image_files = glob.glob(os.path.join(image_dir, "*.jpg"))
         self.image_files = list(sorted(image_files))
@@ -34,8 +35,17 @@ class BaseDataset(Dataset):
         self.label_files = list(sorted(label_files))
 
         self.transforms = None
-        if transforms:
+        if transform_params:
+            transforms = self.get_transforms(transform_params)
             self.transforms = tv2.Compose(transforms)
+
+    def get_transforms(self, params: Dict[str, Dict]) -> List[torch.nn.Module]:
+        transforms_list = []
+        for cls, arg in params.items():
+            transform_cls = importlib.import_module(cls, "data.transforms")
+            transforms_list.append(transform_cls(**arg))
+
+        return transforms_list
 
     def __len__(self) -> int:
         return len(self.image_paths)
@@ -45,28 +55,23 @@ class BaseDataset(Dataset):
 
 
 class LVISDataset(BaseDataset):
-    def __init__(self, image_dir: str, label_dir: str, transforms):
+    def __init__(self, image_dir: str, label_dir: str, transform_params: Optional[Dict[str, Dict]]):
         super().__init__(
             image_dir=image_dir, 
             label_dir=label_dir, 
-            transforms=transforms
+            transform_params=transform_params,
         )
-
-        self._validate_data_files()
-
+        self.validate_data_files()
     
-    def _validate_data_files(self) -> None:
+    def validate_data_files(self) -> None:
         """ Ensure alignment between image and label files. """
         pass
 
     def __getitem__(self, idx: int) -> Tuple[torch.Tensor, torch.Tensor]:
-        # TODO image sizes are not uniform, should be the default transform, I think yolov3 uses 640?
-        # the transforms also need to be applied to the boxes!
         # TODO width/height can be obtained from metadata
         image = read_image(self.image_files[idx])
         
         # TODO labels' xywh dimensions are not normalized
-        # TODO
         label = torch.from_numpy(
             pd.read_csv(self.label_files[idx], header=None, sep=" ").values
         )
