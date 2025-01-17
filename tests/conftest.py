485d7e0861d1b9ce91efef84ffdc192f4ac0dcaf
Chad Gu <chad.m.gu@gmail.com>
2025-01-17 03:20:50 +0000

    Remove tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,7 @@
 import pytest
 import torch
 
-from data.transforms import LabelPreprocessor
-
 
 @pytest.fixture
 def dummy_label() -> torch.Tensor:
     return torch.tensor([1.0, 50.0, 50.0, 100.0, 100.0]).reshape((1, 5))
-
-
-@pytest.fixture
-def label_preprocessor() -> LabelPreprocessor:
-    return LabelPreprocessor(
-        num_classes=42,
-        max_boxes=50,
-        strides=(8, 16, 32)
-    )
