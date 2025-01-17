ed383ce3142bbf497ecb1e6bcff598a938b481b2
Chad Gu <chad.m.gu@gmail.com>
2025-01-17 17:55:49 +0000

    Tests for shapes, loss functions impl. (still have to check object loss)

--- yolo/loss.py
+++ yolo/loss.py
@@ -1,44 +1,14 @@
 import torch
-import torch.nn.functional as F
 
-from torchvision.ops import box_iou, box_convert
 from typing import List, Tuple
-
-
-# TODO anchors need to be scaled in data preprocessing/loss calculation
-all_anchors = [
-    (10, 13, 16, 30, 33, 23), # P3/8
-    (30, 61, 62, 45, 59, 119), # P4/16
-    (116, 90, 156, 198, 373, 326) # P5/32
-]
-
-
-def obj_anchors_ious(    
-    label_box: torch.Tensor, 
-    all_anchors: torch.Tensor,
-):
-    xy = label_box[:, 0:2]
-    label_rect = F.pad(xy, (2, 0))
-    anchor_rect = F.pad(all_anchors.view(-1, 2), (2,0))
-
-    ious = box_iou(
-        boxes1=box_convert(label_rect, "cxcywh", "xyxy"),
-        boxes2=box_convert(anchor_rect, "cxcywh", "xyxy"),
-    )
-    return ious
-
-
-def make_one_hot_label(label: torch.Tensor, num_classes: int):
-    c = label[:, 0].squeeze()
-    one_hot = torch.eye(n=num_classes)
-    return one_hot[c.int()]
-
-
-def make_grid_idx(label: torch.Tensor, stride: int):
-    stride = torch.tensor(stride).float()
-    xy = label[:, 1:3]
-    xy = torch.div(xy - torch.fmod(xy, stride), stride)
-    return xy
+from yolo.loss_utils import (
+    get_box_coordinates, 
+    obj_anchors_ious, 
+    make_grid_idx, 
+    make_one_hot_label, 
+    standardize_label_box,
+    compute_threshold_mask
+)
 
 
 class YOLOLoss(object):
@@ -53,11 +23,6 @@ class YOLOLoss(object):
     ):
         """
         Loss at a single scale/stride. 3 anchors per scale
-
-        Loss terms:
-        1. MSE for bounding box (size = 1)
-        2. BCE for confidence score / IoU (size = 1)
-        3. BCE for classification for class (size = # of classes)
         """
         self.bce = torch.nn.BCEWithLogitsLoss(reduction="none")
         self.mse = torch.nn.MSELoss(reduction="none")
@@ -69,76 +34,11 @@ class YOLOLoss(object):
         anchors = all_anchors[anchors_idx]
         na = len(anchors)
         self.all_anchors = torch.tensor(all_anchors).view(-1, 2)
-        self.anchors = torch.cat([torch.tensor(x) for x in anchors]).view(-1, 2)
-        self.anchor_masks = (anchors_idx + (i+1) * na for i in range(na // 2))
+
+        self.anchors = torch.tensor(anchors).view(-1, 2)
+        self.anchor_masks = [anchors_idx + (i+1) * na for i in range(na // 2)]
 
         self.ignore_threshold = ignore_threshold        
-
-
-    def get_box(self, pred_box):
-        """
-        pred_box: (bs, na, nx, ny, 4)
-        output: (bs, na, nx, ny, 4)
-        """
-        box = torch.zeros_like(pred_box)
-        bs, na, nx, ny = pred_box.shape[0:4]
-
-        grid = torch.zeros((nx, ny))
-        idx = torch.cat(torch.where(grid == 0)).view(2, -1).T.view(1, 1, nx, ny, 2) # could probably be cached
-        offset = idx.repeat(bs, na, 1, 1, 1)
-
-        anchors = self.anchors.view(1, na, 1, 1, 2)
-        anchors = anchors.repeat(bs, 1, nx, ny, 1)
-
-        box[..., 0:2] = pred_box[..., 0:2].sigmoid() + offset * self.stride
-        box[..., 2:4] = anchors * torch.exp(pred_box[..., 2:4])
- 
-        return box
-
-
-    def compute_threshold_mask(
-        self, 
-        pred_box: torch.Tensor, 
-        label_box: torch.Tensor, 
-    ) -> torch.Tensor:
-        """
-        Returns a boolean mask computed by overlapping the predictions in each cell + anchor
-        with the ground truth bounding box.
-
-        For each object + anchor/cell pair, the best IoUs below a threshold are kept.
-
-        pred_box: (bs, na, nx, ny, 4)
-        label_box: (?, 4), ? is the # of objects in the batch
-
-        :return: Boolean tensor of shape (bs, na, nx, ny)
-        """
-
-        bs, na, nx, ny, _ = pred_box.shape
-
-        ious = box_iou(
-            boxes1=box_convert(pred_box.view(-1, 4), "cxcywh", "xyxy"),
-            boxes2=box_convert(label_box.view(-1, 4), "cxcywh", "xyxy"),
-        ) # (bs * na * nx * ny, ?)
-        best_ious = ious.amax(dim=-1).view(bs, na, nx, ny)
-        mask = best_ious < self.ignore_threshold
-
-        return mask
-    
-
-    def standardize_label_box(self, label_box: torch.Tensor, best_idx: torch.Tensor) -> torch.Tensor:
-        """
-        Standardizes box coordinates. (x, y) is measured as the offset to
-        nearest cell + normalized by stride. (w, h) are exponents.
-        """
-        x, y, w, h = label_box.T # shapes = (?, 1)
-        shape = label_box.T.shape
-        s = torch.tensor(self.stride).float()
-
-        x = torch.div(torch.fmod(x, s), s)
-        y = torch.div(torch.fmod(y, s), s)
-        w = torch.log(w / self.anchors[best_idx, 0] + 1e-16)
-        h = torch.log(h / self.anchors[best_idx, 1] + 1e-16)
-        return torch.cat([x, y, w, h]).view(shape).T
 
 
     def __call__(self, preds: torch.Tensor, label: torch.Tensor):
@@ -148,21 +48,21 @@ class YOLOLoss(object):
 
         bs, na, nx, ny, _ = preds.shape
 
-        pred_box, pred_obj, pred_class = torch.split(preds, split_size_or_sections=[4, 1, self.num_class], dim=-1)
+        pred_box, pred_obj, pred_class = torch.split(preds, split_size_or_sections=[4, 1, self.num_classes], dim=-1)
         batch_idx, label = torch.split(label, split_size_or_sections=[1, 5], dim=-1)
         batch_idx = batch_idx.view(-1, 1)
 
-        pred_box = self.get_box(pred_box)
-        label_box = label[:, 2:] # (?, 4)
-        threshold_mask = self.compute_threshold_mask(pred_box, label_box)
+        pred_box = get_box_coordinates(pred_box, self.anchors, self.stride)
+        label_box = label[:, 1:] # (?, 4)
+        threshold_mask = compute_threshold_mask(pred_box, label_box, self.ignore_threshold)
 
         ious = obj_anchors_ious(label_box, self.all_anchors)
         best_anchor_idx = torch.argmax(ious, dim=-1).view(-1, 1)
-        # Used to filter out the ? objects in the batch
         obj_mask = (best_anchor_idx >= self.anchor_masks[0]) * (best_anchor_idx <= self.anchor_masks[-1])
+        obj_mask = obj_mask.squeeze()
 
         grid_idx = make_grid_idx(label, self.stride)
-        label_class = make_one_hot_label(label, batch_idx, self.num_classes)
+        label_class = make_one_hot_label(label, self.num_classes)
         
         batch_idx = batch_idx[obj_mask]
         grid_idx = grid_idx[obj_mask]
@@ -175,6 +75,7 @@ class YOLOLoss(object):
         
         label_box = label_box[obj_mask]
         label_class = label_class[obj_mask]
+
         label_obj = torch.sparse_coo_tensor(
             indices=merged_idx.T,
             values=torch.ones(size=(len(merged_idx), )),
@@ -182,8 +83,14 @@ class YOLOLoss(object):
         ).to_dense()
         mask = label_obj.bool()
 
-        object_loss = threshold_mask[mask] * self.bce(pred_obj[mask], label_obj)        
-        class_loss = self.bce(pred_class[mask], label_class)
-        box_loss = self.mse(pred_box[mask], self.standardize_label_box(label_box, best_anchor_idx))
+        # NOTE wtf, label_obj[mask] are suppsoed to be all 1s? that can't be right?
+        object_loss = threshold_mask[mask] * self.bce(pred_obj[mask].squeeze(), label_obj[mask])
+        object_loss = object_loss.sum() # sum or mean?
+        class_loss = self.bce(pred_class[mask], label_class).sum()
+        box_loss = self.mse(
+            pred_box[mask], 
+            standardize_label_box(label_box, best_anchor_idx, self.anchors, self.stride)
+        ).sum()
 
         return box_loss + class_loss + object_loss
+    
---
+++ yolo/loss_utils.py
@@ -0,0 +1,89 @@
+import torch
+import torch.nn.functional as F
+
+from torchvision.ops import box_iou, box_convert
+
+
+def get_box_coordinates(pred_box: torch.Tensor, anchors: torch.Tensor, stride: int):
+    """
+    Computes box coordinates at each (batch, anchor, cell) index given predictions [tx ty tw th].
+    """
+    box = torch.zeros_like(pred_box)
+    bs, na, nx, ny = pred_box.shape[0:4]
+
+    grid = torch.zeros((nx, ny))
+    idx = torch.cat(torch.where(grid == 0)).view(2, -1).T.view(1, 1, nx, ny, 2) # could probably be cached
+    offset = idx.repeat(bs, na, 1, 1, 1)
+
+    anchors = anchors.view(1, na, 1, 1, 2)
+    anchors = anchors.repeat(bs, 1, nx, ny, 1)
+
+    box[..., 0:2] = pred_box[..., 0:2].sigmoid() + offset * stride
+    box[..., 2:4] = anchors * torch.exp(pred_box[..., 2:4])
+
+    return box
+
+
+def standardize_label_box(label_box: torch.Tensor, best_idx: torch.Tensor, anchors: torch.Tensor, stride: int) -> torch.Tensor:
+    """
+    Standardizes box coordinates. (x, y) is measured as the offset to the 
+    nearest cell and is normalized by stride, while (w, h) are arguments to the exponents.
+    """
+    x, y, w, h = label_box.T # shapes = (?, 1)
+    shape = label_box.T.shape
+    s = torch.tensor(stride).float()
+
+    x = torch.div(torch.fmod(x, s), s)
+    y = torch.div(torch.fmod(y, s), s)
+    w = torch.log(w / anchors[best_idx.squeeze(), 0] + 1e-16)
+    h = torch.log(h / anchors[best_idx.squeeze(), 1] + 1e-16)
+
+    return torch.cat([x, y, w, h]).view(shape).T
+
+
+def compute_threshold_mask(pred_box: torch.Tensor, label_box: torch.Tensor, ignore_threshold: float) -> torch.Tensor:
+    """
+    Returns a boolean mask computed by overlapping the predictions in each cell
+    with the ground truth bounding box. Each cell is responsible for `na` predictions equal
+    to the number of anchor boxes.
+
+    For each object + anchor/cell pair, the best IoUs below the specified threshold are kept.
+    """
+    bs, na, nx, ny, _ = pred_box.shape
+    ious = box_iou(
+        boxes1=box_convert(pred_box.view(-1, 4), "cxcywh", "xyxy"),
+        boxes2=box_convert(label_box.view(-1, 4), "cxcywh", "xyxy"),
+    ) # (bs * na * nx * ny, ?)
+    best_ious = ious.amax(dim=-1).view(bs, na, nx, ny)
+    mask = best_ious < ignore_threshold
+
+    return mask
+
+
+def obj_anchors_ious(label_box: torch.Tensor, all_anchors: torch.Tensor):
+    """
+    Computes the IoUs between all objects in a batch vs all anchor boxes
+    (across all prediction scales).
+    """
+    xy = label_box[:, 0:2]
+    label_rect = F.pad(xy, (2, 0))
+    anchor_rect = F.pad(all_anchors.view(-1, 2), (2,0))
+
+    ious = box_iou(
+        boxes1=box_convert(label_rect, "cxcywh", "xyxy"),
+        boxes2=box_convert(anchor_rect, "cxcywh", "xyxy"),
+    )
+    return ious
+
+
+def make_one_hot_label(label: torch.Tensor, num_classes: int):
+    c = label[:, 0].squeeze()
+    one_hot = torch.eye(n=num_classes)
+    return one_hot[c.int()]
+
+
+def make_grid_idx(label: torch.Tensor, stride: int):
+    stride = torch.tensor(stride).float()
+    xy = label[:, 1:3]
+    xy = torch.div(xy - torch.fmod(xy, stride), stride)
+    return xy
