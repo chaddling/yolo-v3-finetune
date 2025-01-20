0fa141cd18adf74c8aa674d4ba3b835eef952dd2
Chad Gu <chad.m.gu@gmail.com>
2025-01-20 17:34:32 +0000

    Getting train step to run

--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-import argparse
+import yaml
 
 import lightning as L
 
@@ -7,5 +7,25 @@ from yolo.model import PretrainedYOLOModel, ModelWrapper
 
 
 if __name__ == "__main__":
-    trainer = L.Trainer()
-    trainer.fit()
+    with open("./config.yaml", "r") as f:
+        config = yaml.load(f, Loader=yaml.SafeLoader)
+
+    dataloader_config = config.pop("dataloader")
+    train_loader = create_dataloader(dataloader_config, split="train")
+    #val_loader = create_dataloader(dataloader_config, split="val")
+
+    train_config = config.pop("train")
+    model = PretrainedYOLOModel(
+        weights_dir="./weights",
+        num_classes=1203,
+        anchors=train_config["anchors"],
+        strides=train_config["strides"],
+    )
+    wrapped_model = ModelWrapper(model, input_image_size=train_config["input_image_size"])
+
+    trainer = L.Trainer(
+        accelerator="gpu",
+        limit_train_batches=1,
+        max_epochs=5,
+    )
+    trainer.fit(wrapped_model, train_loader)
--- yolo/model.py
+++ yolo/model.py
@@ -9,25 +9,21 @@ from yolo.loss import YOLOLoss
 from typing import List, Tuple
 
 # TODO anchors need to be scaled in loss calculation
-ANCHORS = [
-    (10, 13, 16, 30, 33, 23), # stride 8
-    (30, 61, 62, 45, 59, 119), # stride 16
-    (116, 90, 156, 198, 373, 326) # stride 32
-]
-STRIDE = (8, 16, 32)
 CONNECT_FROM = (27, 22, 15)
 
 class PretrainedYOLOModel(torch.nn.Module):
     def __init__(self, 
         weights_dir: str, 
         num_classes: int, # num_classes = # of classes in LVIS
-        anchors: List[Tuple[int]] = ANCHORS, 
+        anchors: List[List[int]], # TODO anchors need to be scaled in loss calculation
+        strides: List[int],
         training: bool = True
     ):
         super().__init__()
 
         # Load custom weights, downloaded to `weights_dir`
         # TODO add function to get the weights
+        # NOTE it seems like by default these loaded parameters are frozen
         self._model = torch.hub.load(
             "ultralytics/yolov3", 
             "custom", 
@@ -41,16 +37,20 @@ class PretrainedYOLOModel(torch.nn.Module):
         detect_layer = Detect(
             nc=num_classes, 
             anchors=anchors,
-            ch=[len(anchors)],
+            ch=[256, 512, 1024], # NOTE These are hardcoded to input size = 512
         )
         # These attributes are needed in the BaseModel's _forward_once() instructions
-        detect_layer.stride = STRIDE
+        detect_layer.stride = strides
         detect_layer.i = len(self._model.model.model)
         detect_layer.type = "yolo.models.Detect"
         detect_layer.f = CONNECT_FROM
         detect_layer.np = sum(x.numel() for x in detect_layer.parameters())
 
-        self._model.append(detect_layer)
+        self._model.model.model.append(detect_layer)
+
+        self.num_classes = num_classes
+        self.anchors = anchors
+        self.strides = strides
 
     def forward(self, x):
         x = self._model(x)
@@ -58,32 +58,32 @@ class PretrainedYOLOModel(torch.nn.Module):
     
 
 class ModelWrapper(L.LightningModule):
-    def __init__(self, model: torch.nn.Module):
+    def __init__(self, model: torch.nn.Module, input_image_size: int):
         super().__init__()
         self.model = model
-
-        self.num_classes = model.num_classes
-
-        self.strides = (8, 16, 32)
-
         self.yolo_loss = [
-            YOLOLoss(self.num_classes, ANCHORS, i, s) 
-            for i, s in enumerate(self.strides)
+            YOLOLoss(
+                num_classes=model.num_classes,
+                all_anchors=model.anchors, 
+                anchors_idx=i, 
+                stride=s,
+                input_image_size=input_image_size,
+            ) for i, s in enumerate(model.strides)
         ]
 
     def training_step(self, batch, batch_idx):
         inputs, label = batch
-        preds = self.model(inputs)
+        preds = self.model(inputs.float())
 
         total_loss = 0
         for pred, loss in zip(preds, self.yolo_loss):
-            total_loss += loss(pred, label)
+            total_loss += loss(pred, label.float())
 
         return total_loss
     
-    def validation_step(self, batch, batch_idx):
-        return
+    # def validation_step(self, batch, batch_idx):
+    #     return
     
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        optimizer = torch.optim.Adam(self.model.parameters(), lr=1e-3)
         return optimizer
