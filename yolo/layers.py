9a549024e0f398eaa0236db16eacee614efc6d8e
Chad Gu <chad.m.gu@gmail.com>
2025-01-24 19:59:06 +0000

    Removing some custom transformations for eval

--- yolo/layers.py
+++ yolo/layers.py
@@ -26,7 +26,6 @@ class Detect(torch.nn.Module):
 
         Expects x as list of tensors with shape(bs, C, H, W).
         """
-        z = []  # inference output
         for i in range(self.nl):
             x[i] = self.m[i](x[i])  # conv 
             bs, _, ny, nx = x[i].shape  # x(bs,255,20,20) to x(bs,3,20,20,85)
@@ -34,34 +33,4 @@ class Detect(torch.nn.Module):
             # output order: [batch_size, anchor, nx, ny, 5+nc]
             x[i] = x[i].view(bs, self.na, self.no, ny, nx).permute(0, 1, 3, 4, 2).contiguous()
 
-            if not self.training:  # inference
-                if self.dynamic or self.grid[i].shape[2:4] != x[i].shape[2:4]:
-                    self.grid[i], self.anchor_grid[i] = self._make_grid(nx, ny, i)
-
-                # Detect (boxes only)
-                xy, wh, conf = x[i].sigmoid().split((2, 2, self.nc + 1), 4)
-                xy = (xy * 2 + self.grid[i]) * self.stride[i]  # xy
-                wh = (wh * 2) ** 2 * self.anchor_grid[i]  # wh
-                y = torch.cat((xy, wh, conf), 4)
-                z.append(y.view(bs, self.na * nx * ny, self.no))
-
-        return x if self.training else (torch.cat(z, 1),) if self.export else (torch.cat(z, 1), x)
-
-    def _make_grid(self, nx=20, ny=20, i=0):
-        """Generates a grid and corresponding anchor grid with shape `(1, num_anchors, ny, nx, 2)` for indexing
-        anchors.
-        """
-        d = self.anchors[i].device
-        t = self.anchors[i].dtype
-        shape = 1, self.na, ny, nx, 2  # grid shape
-        y, x = torch.arange(ny, device=d, dtype=t), torch.arange(nx, device=d, dtype=t)
-
-        # torch 1.10.0 compatibility
-        if torch.__version__ > "1.10.0":
-            yv, xv = torch.meshgrid(y, x, indexing="ij")
-        else:
-            yv, xv = torch.meshgrid(y, x)
-
-        grid = torch.stack((xv, yv), 2).expand(shape) - 0.5  # add grid offset, i.e. y = 2.0 * x - 0.5
-        anchor_grid = (self.anchors[i] * self.stride[i]).view((1, self.na, 1, 1, 2)).expand(shape)
-        return grid, anchor_grid
+        return x
