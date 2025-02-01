16118d0d8c2c8685f88130b03ae3164f206e4f52
Chad Gu <chad.m.gu@gmail.com>
2025-02-01 18:26:45 -0500

    merge category names and ids

---
+++ scripts/merge_labels.py
@@ -0,0 +1,143 @@
+import json
+import logging
+import os
+import pandas as pd
+
+logging.getLogger().setLevel(logging.INFO)
+
+HOME = os.getenv("HOME")
+
+# This mapping is extracted from the official LVIS mapping:
+# https://github.com/lvis-dataset/lvis-api/blob/master/data/coco_to_synset.json
+COCO_SYNSET_CATEGORIES = [
+    {"synset": "person.n.01", "coco_cat_id": 1},
+    {"synset": "bicycle.n.01", "coco_cat_id": 2},
+    {"synset": "car.n.01", "coco_cat_id": 3},
+    {"synset": "motorcycle.n.01", "coco_cat_id": 4},
+    {"synset": "airplane.n.01", "coco_cat_id": 5},
+    {"synset": "bus.n.01", "coco_cat_id": 6},
+    {"synset": "train.n.01", "coco_cat_id": 7},
+    {"synset": "truck.n.01", "coco_cat_id": 8},
+    {"synset": "boat.n.01", "coco_cat_id": 9},
+    {"synset": "traffic_light.n.01", "coco_cat_id": 10},
+    {"synset": "fireplug.n.01", "coco_cat_id": 11},
+    {"synset": "stop_sign.n.01", "coco_cat_id": 13},
+    {"synset": "parking_meter.n.01", "coco_cat_id": 14},
+    {"synset": "bench.n.01", "coco_cat_id": 15},
+    {"synset": "bird.n.01", "coco_cat_id": 16},
+    {"synset": "cat.n.01", "coco_cat_id": 17},
+    {"synset": "dog.n.01", "coco_cat_id": 18},
+    {"synset": "horse.n.01", "coco_cat_id": 19},
+    {"synset": "sheep.n.01", "coco_cat_id": 20},
+    {"synset": "beef.n.01", "coco_cat_id": 21},
+    {"synset": "elephant.n.01", "coco_cat_id": 22},
+    {"synset": "bear.n.01", "coco_cat_id": 23},
+    {"synset": "zebra.n.01", "coco_cat_id": 24},
+    {"synset": "giraffe.n.01", "coco_cat_id": 25},
+    {"synset": "backpack.n.01", "coco_cat_id": 27},
+    {"synset": "umbrella.n.01", "coco_cat_id": 28},
+    {"synset": "bag.n.04", "coco_cat_id": 31},
+    {"synset": "necktie.n.01", "coco_cat_id": 32},
+    {"synset": "bag.n.06", "coco_cat_id": 33},
+    {"synset": "frisbee.n.01", "coco_cat_id": 34},
+    {"synset": "ski.n.01", "coco_cat_id": 35},
+    {"synset": "snowboard.n.01", "coco_cat_id": 36},
+    {"synset": "ball.n.06", "coco_cat_id": 37},
+    {"synset": "kite.n.03", "coco_cat_id": 38},
+    {"synset": "baseball_bat.n.01", "coco_cat_id": 39},
+    {"synset": "baseball_glove.n.01", "coco_cat_id": 40},
+    {"synset": "skateboard.n.01", "coco_cat_id": 41},
+    {"synset": "surfboard.n.01", "coco_cat_id": 42},
+    {"synset": "tennis_racket.n.01", "coco_cat_id": 43},
+    {"synset": "bottle.n.01", "coco_cat_id": 44},
+    {"synset": "wineglass.n.01", "coco_cat_id": 46},
+    {"synset": "cup.n.01", "coco_cat_id": 47},
+    {"synset": "fork.n.01", "coco_cat_id": 48},
+    {"synset": "knife.n.01", "coco_cat_id": 49},
+    {"synset": "spoon.n.01", "coco_cat_id": 50},
+    {"synset": "bowl.n.03", "coco_cat_id": 51},
+    {"synset": "banana.n.02", "coco_cat_id": 52},
+    {"synset": "apple.n.01", "coco_cat_id": 53},
+    {"synset": "sandwich.n.01", "coco_cat_id": 54},
+    {"synset": "orange.n.01", "coco_cat_id": 55},
+    {"synset": "broccoli.n.01", "coco_cat_id": 56},
+    {"synset": "carrot.n.01", "coco_cat_id": 57},
+    {"synset": "frank.n.02", "coco_cat_id": 58},
+    {"synset": "pizza.n.01", "coco_cat_id": 59},
+    {"synset": "doughnut.n.02", "coco_cat_id": 60},
+    {"synset": "cake.n.03", "coco_cat_id": 61},
+    {"synset": "chair.n.01", "coco_cat_id": 62},
+    {"synset": "sofa.n.01", "coco_cat_id": 63},
+    {"synset": "pot.n.04", "coco_cat_id": 64},
+    {"synset": "bed.n.01", "coco_cat_id": 65},
+    {"synset": "dining_table.n.01", "coco_cat_id": 67},
+    {"synset": "toilet.n.02", "coco_cat_id": 70},
+    {"synset": "television_receiver.n.01", "coco_cat_id": 72},
+    {"synset": "laptop.n.01", "coco_cat_id": 73},
+    {"synset": "mouse.n.04", "coco_cat_id": 74},
+    {"synset": "remote_control.n.01", "coco_cat_id": 75},
+    {"synset": "computer_keyboard.n.01", "coco_cat_id": 76},
+    {"synset": "cellular_telephone.n.01", "coco_cat_id": 77},
+    {"synset": "microwave.n.02", "coco_cat_id": 78},
+    {"synset": "oven.n.01", "coco_cat_id": 79},
+    {"synset": "toaster.n.02", "coco_cat_id": 80},
+    {"synset": "sink.n.01", "coco_cat_id": 81},
+    {"synset": "electric_refrigerator.n.01", "coco_cat_id": 82},
+    {"synset": "book.n.01", "coco_cat_id": 84},
+    {"synset": "clock.n.01", "coco_cat_id": 85},
+    {"synset": "vase.n.01", "coco_cat_id": 86},
+    {"synset": "scissors.n.01", "coco_cat_id": 87},
+    {"synset": "teddy.n.01", "coco_cat_id": 88},
+    {"synset": "hand_blower.n.01", "coco_cat_id": 89},
+    {"synset": "toothbrush.n.01", "coco_cat_id": 90},
+]
+
+
+if __name__ == "__main__":
+    lvis_train = os.path.join(HOME, "lvis_v1_train.json")
+    try:
+        with open(lvis_train, "r") as f:
+            metadata = json.load(f)
+    except FileNotFoundError as e:
+        raise(e)
+    
+    categories = metadata["categories"]
+    df = pd.DataFrame.from_records(categories)
+    df = df.set_index("synset")
+
+    merged_vocabs = []
+    unmerged_synset = []
+    idx = 1
+    for c in COCO_SYNSET_CATEGORIES:
+        synset = c["synset"]
+        try:
+            vocab = df.loc[synset, "name"]
+        except KeyError:
+            unmerged_synset.append(c)
+            logging.info(f"Unmerged COCO synset: {c}.")
+
+        merged_vocabs.append({"name": vocab, "synset": synset, "coco_id": c["coco_cat_id"], "lvis_id": None, "merged_id": idx})
+        idx += 1
+
+
+    logging.info(f"Found {len(unmerged_synset)} from COCO_SYNSET_CATEGORIES.")
+
+    merged_synset = [x["synset"] for x in merged_vocabs]
+    df = df.loc[[x for x in df.index if x not in merged_synset]]
+
+    for synset, row in df.iterrows():
+        merged_vocabs.append(
+            {
+                "name": row["name"],
+                "synset": synset,
+                "coco_id": None,
+                "lvis_id": row["id"],
+                "merged_id": idx,
+            }
+        )
+        idx += 1
+
+    logging.info(f"Final number of categories after merging: {len(merged_vocabs)}.")
+
+    # TODO now we have to edit and merge the annotations in the images
+    # using the merged labels
